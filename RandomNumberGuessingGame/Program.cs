381df29550d33db29e7aaa6426015ca56a95b57b
Billy Gordon <Billywgordon93@gmail.com>
2020-11-26 00:10:40 -0600

    Gave user the ability for continous play, and abstracted logic into methods.

--- RandomNumberGuessingGame/Program.cs
+++ RandomNumberGuessingGame/Program.cs
@@ -5,36 +5,62 @@ namespace RandomNumberGuessingGame
 {
     class Program
     {
+        static int randomNumber = 0;
         static Random rng = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("I am the number master!"); 
-            Console.WriteLine("Welcome to the random number guessing game");
-            Console.WriteLine("Choose a number between 1 and 100");
-            
-            int randomNumber = rng.Next(1, 101);
-            int userNumber = int.Parse(Console.ReadLine());
+                bool playAgain = true;
             do
-            {               
-                if (userNumber < randomNumber)
+            {
+                WelcomeText();
+                randomNumber = GetRandomNumber();
+                int userNumber = GetUserNumber(Console.ReadLine());
+
+                while (userNumber != randomNumber)
                 {
-                    Console.WriteLine("To low, guess again!");
-                    userNumber = int.Parse(Console.ReadLine());
+                    CheckUserGuess(ref userNumber, randomNumber);
                 }
-                else if (userNumber > randomNumber)
+
+                Console.WriteLine("You were correct!");
+                Console.WriteLine($"Would you like to play again? yes/no");
+                if (Console.ReadLine().ToLower() == "yes")
                 {
-                    Console.WriteLine("To high, guess again!");
-                    userNumber = int.Parse(Console.ReadLine());
+                    playAgain = true;
                 }
                 else
                 {
-                    Console.WriteLine("You were incorrect!");
-                    Console.WriteLine("The correct number was: " + randomNumber);
+                    playAgain = false;
                 }
-            } while (userNumber != randomNumber);
-            
-             Console.WriteLine("You were correct!");
-            
+            } while (playAgain == true);
+        }
+
+        private static int GetRandomNumber()
+        {
+            return rng.Next(1, 101);
+        }
+
+        static int GetUserNumber(string userInput)
+        {
+            return int.Parse(userInput);
+        }
+        static void CheckUserGuess(ref int userNumber, int randomNumber)
+        {
+            if (userNumber < randomNumber)
+            {
+                Console.WriteLine("To low, guess again!");
+                userNumber = GetUserNumber(Console.ReadLine());
+            }
+            else if (userNumber > randomNumber)
+            {
+                Console.WriteLine("To high, guess again!");
+                userNumber = GetUserNumber(Console.ReadLine());
+            }
+        }
+        static void WelcomeText()
+        {
+            Console.WriteLine("I am the number master!");
+            Console.WriteLine("Welcome to the random number guessing game");
+            Console.WriteLine("Choose a number between 1 and 100");
         }
     }
 }
